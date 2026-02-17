83d3516f11f2084e9d4e155680a1a07a4db08906
Diogo Major Vieira <diogomajor1993@outlook.pt>
2026-02-17 12:32:21 +0000

    Add project files.

---
+++ Projeto2_NIF_Web_Razor/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Projeto2_NIF_Web_Razor.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ Projeto2_NIF_Web_Razor/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Projeto2_NIF_Web_Razor.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
